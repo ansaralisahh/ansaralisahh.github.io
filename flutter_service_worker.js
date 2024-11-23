606159e0e026ccb9ec35eb45c4f9ec3dbc78feed
Ansar Ali Shah <120328821+ansaralisah@users.noreply.github.com>
2024-11-23 12:33:04 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "da2a93536d1932c4089a17aabed2c6ec
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "757e7d3515a2d639dd673b22fc37da8d",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "36987315e76d66afbab93f5039492962",
+"flutter_bootstrap.js": "ec14b2618e6bc434477643f02ad1f173",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
